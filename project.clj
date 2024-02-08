5a32801f40764038a753055a5ada740697fbb90e
csdream <csdream@gmail.com>
2024-02-09 05:49:24 +0900

    Add MIT License to publish to Clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject org.clojars.csdream/lein-elastic-beanstalk "0.2.9-SNAPSHOT"
   :description "Leiningen plugin for Amazon's Elastic Beanstalk"
   :url "https://github.com/csdream/lein-elastic-beanstalk"
+  :license {:name "MIT"
+            :url "https://choosealicense.com/licenses/mit"
+            :key "mit"
+            :year 2024
+            :comment "MIT License"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [com.amazonaws/aws-java-sdk "1.6.10"]
                  [lein-ring "0.8.2"]]
